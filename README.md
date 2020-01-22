d8f73a0d3930f96cd9c800f1c22ec1d412223e0e
Brian Leishman <brian@stumpyinc.com>
2020-01-22 18:58:36 -0500

    v1

--- README.md
+++ README.md
@@ -1,1 +1,97 @@
-# mysql-trimmean
+# MySQL TRIMMEAN
+
+Adds a MySQL function to exactly match Excel's `TRIMNMEAN` function. https://support.office.com/en-us/article/trimmean-function-d90c9878-a119-4746-88fa-63d988f511d3
+
+> Returns the mean of the interior of a data set. TRIMMEAN calculates the mean taken by excluding a percentage of data points from the top and bottom tails of a data set. You can use this function when you wish to exclude outlying data from your analysis.
+
+## Usage
+
+```sql
+`trimmean` ( `NumberColumn`, double `Percent` [, integer `Decimals` = 4 ] )
+```
+
+ - `` `NumberColumn` ``
+   - The column of values to trim and average.
+
+ - `` `Percent` ``
+   - The fractional number of data points to exclude from the calculation. For example, if percent = 0.2, 4 points are trimmed from a data set of 20 points (20 x 0.2): 2 from the top and 2 from the bottom of the set.
+
+ - `` `Decimals` ``
+   - Optionally, the number of decimal places to output. Default is 4.
+
+## Examples
+
+We can quickly build a test table of random numbers by running these queries
+
+```sql
+-- build the table for random numbers
+create table`rand_numbers`(
+    `Number`int not null
+);
+-- seed the table with the first row
+insert into`rand_numbers`(`Number`)values(rand()*3333);
+-- run this one 5 times to create 32 rows of random numbers
+insert into`rand_numbers`(`Number`)select`Number`*rand()from`rand_numbers`;
+insert into`rand_numbers`(`Number`)select`Number`*rand()from`rand_numbers`;
+insert into`rand_numbers`(`Number`)select`Number`*rand()from`rand_numbers`;
+insert into`rand_numbers`(`Number`)select`Number`*rand()from`rand_numbers`;
+insert into`rand_numbers`(`Number`)select`Number`*rand()from`rand_numbers`;
+```
+
+Then use this query to see the average and trimmed average, removing 20% of the values from the average (10% from the bottom and 10% from the top) in this example
+
+```sql
+select avg(`Number`)`avg`,
+`trimmean`(`Number`,.2)`trimmean`
+from`rand_numbers`;
+```
+
+Which returns this table (your numbers will likely be different since the table was randomly generated)
+
+| avg | trimmean|
+| --- | --- |
+| 702.5625 | 614.8846 |
+
+We can even verify that this matches exactly by running this query
+
+```sql
+select*from`rand_numbers`;
+```
+
+and pasting the output into Excel, passing the same exact numbers through Excel's `TRIMMEAN` function
+
+> **Note**: you can use the optional 3rd parameter of the MySQL `TRIMMEAN` function to control the number of decimals, up to 16 decimals
+
+![excel](excel.png)
+
+## Dependencies
+
+Debian / Ubuntu
+
+```shell
+sudo apt update
+sudo apt install libmysqlclient-dev
+```
+
+## Installing
+
+You can find your MySQL plugin directory by running this MySQL query
+
+```sql
+select @@plugin_dir;
+```
+
+then replace `/usr/lib/mysql/plugin` below with your MySQL plugin directory.
+
+```shell
+git clone https://github.com/StirlingMarketingGroup/mysql-trimmean.git
+cd mysql-trimmean
+gcc -O3 -I/usr/include/mysql -o trimmean.so -shared trimmean.c -fPIC
+sudo cp trimmean.so /usr/lib/mysql/plugin/trimmean.so
+```
+
+Enable the function in MySQL by running this MySQL query
+
+```sql
+create aggregate function`trimmean`returns real soname'trimmean.so';
+```
---
+++ trimmean.c
@@ -0,0 +1,147 @@
+#ifdef STANDARD
+/* STANDARD is defined, don't use any mysql functions */
+#include <string.h>
+#ifdef __WIN__
+typedef unsigned __int64 ulonglong; /* Microsofts 64 bit types */
+typedef __int64 longlong;
+#else
+typedef unsigned long long ulonglong;
+typedef long long longlong;
+#endif /*__WIN__*/
+#else
+#include <my_global.h>
+#include <my_sys.h>
+#if defined(MYSQL_SERVER)
+#include <m_string.h>
+#else
+/* when compiled as standalone */
+#include <string.h>
+#endif
+#endif
+#include <mysql.h>
+#include <stdint.h>
+
+#define BUFFERSIZE 1024
+
+my_bool trimmean_init(UDF_INIT *initid, UDF_ARGS *args, char *message);
+void trimmean_deinit(UDF_INIT *initid);
+void trimmean_clear(UDF_INIT *initid, char *is_null, char *is_error);
+void trimmean_reset(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char *error);
+void trimmean_add(UDF_INIT* initid, UDF_ARGS* args, char* is_null, char *error);
+double trimmean(UDF_INIT *initid __attribute__ ((unused)), UDF_ARGS *args, char *is_null, char *is_error);
+
+
+struct trimmean_data {
+  unsigned long count;
+  unsigned long abscount;
+  unsigned long pages;
+  double *values;
+};
+
+
+my_bool trimmean_init(UDF_INIT *initid, UDF_ARGS *args, char *message) {
+	if (args->arg_count < 2 || args->arg_count > 3) {
+		strcpy(message, "`trimmean`() requires 2 parameter: the values to trim and average, and the fractional number of data points to exclude from the calculation");
+		return 1;
+	}
+
+	args->arg_type[0] = REAL_RESULT;
+    args->arg_type[1] = REAL_RESULT;
+
+    args->arg_type[2] = INT_RESULT;
+
+    struct trimmean_data *buffer = malloc(sizeof(struct trimmean_data));
+    buffer->count = 0;
+    buffer->abscount = 0;
+    buffer->pages = 1;
+    buffer->values = NULL;
+
+    initid->decimals = 4;
+    if (args->arg_count == 3 && (*((unsigned int*)args->args[2]) <= 16)) {
+        initid->decimals = *((unsigned int*)args->args[2]);
+    }
+
+	initid->maybe_null = 1; // can return null
+    initid->max_length  = 32;
+    initid->ptr = (char *)buffer;
+
+	return 0;
+}
+
+void trimmean_deinit(UDF_INIT* initid) {
+    struct trimmean_data *buffer = (struct trimmean_data*)initid->ptr;
+
+    if (buffer->values != NULL) {
+        free(buffer->values);
+        buffer->values = NULL;
+    }
+    free(initid->ptr);
+    initid->ptr = NULL;
+}
+
+void trimmean_clear(UDF_INIT *initid, char *is_null, char *is_error) {
+    struct trimmean_data *buffer = (struct trimmean_data*)initid->ptr;
+    buffer->count = 0;
+    buffer->abscount = 0;
+    buffer->pages = 1;
+    *is_null = 0;
+    *is_error = 0;
+
+    if (buffer->values != NULL) {
+        free(buffer->values);
+        buffer->values = NULL;
+    }
+
+    buffer->values=(double *) malloc(BUFFERSIZE*sizeof(double));
+}
+
+void trimmean_reset(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *is_error) {
+    trimmean_clear(initid, is_null, is_error);
+    trimmean_add(initid, args, is_null, is_error);
+}
+
+void trimmean_add(UDF_INIT *initid, UDF_ARGS *args, char *is_null, char *is_error) {
+    if (args->args[0] != NULL) {
+        struct trimmean_data *buffer = (struct trimmean_data*)initid->ptr;
+        if (buffer->count >= BUFFERSIZE) {
+            buffer->pages++;
+            buffer->count = 0;
+            buffer->values = (double *) realloc(buffer->values,BUFFERSIZE*buffer->pages*sizeof(double));
+        }
+        buffer->values[buffer->abscount++] = *((double*)args->args[0]);
+        buffer->count++;
+    }
+}
+
+int compare_doubles (const void *a, const void *b) {
+  const double *da = (const double *) a;
+  const double *db = (const double *) b;
+
+  return (*da > *db) - (*da < *db);
+}
+
+double trimmean(UDF_INIT *initid __attribute__ ((unused)), UDF_ARGS *args, char *is_null, char *is_error) {
+    struct trimmean_data* buffer = (struct trimmean_data*)initid->ptr;
+
+    if (buffer->abscount == 0 || *is_error != 0) {
+        *is_null = 1;
+        return 0.0;
+    }
+
+    *is_null = 0;
+    if (buffer->abscount == 1) {
+        return buffer->values[0];
+    }
+
+    qsort(buffer->values, buffer->abscount, sizeof(double), compare_doubles);
+
+    int trim = (int)(*((double*)args->args[1]) * buffer->abscount / 2);
+
+    double sum = 0.0;
+    int i;
+    for (i = trim; i < buffer->abscount-trim; i++) {
+        sum += buffer->values[i];
+    }
+
+	return sum/(buffer->abscount-trim*2);
+}
